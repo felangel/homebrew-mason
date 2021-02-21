bfbcecb3344859a744a1eba212d116a9c9503bff
Felix Angelov <felangelov@gmail.com>
2021-02-21 00:08:12 -0600

    feat: add mason formula

---
+++ mason.rb
@@ -0,0 +1,36 @@
+class Mason < Formula
+  desc "A template generator which helps teams generate files quickly and consistently."
+  homepage "https://pub.dev/packages/mason"
+  url "https://github.com/felangel/mason/archive/v0.0.1-dev.27.tar.gz"
+  sha256 "95c265547d6565e9591b7e9130f6877fcf3b4709a72793f5f7633f82ecd97fc3"
+  license "MIT"
+
+  depends_on "dart"
+
+  def install
+    dart = Formula["dart"].opt_bin
+
+    pubspec = YAML.safe_load(File.read("pubspec.yaml"))
+    version = pubspec["version"]
+
+    # Tell the pub server where these installations are coming from.
+    ENV["PUB_ENVIRONMENT"] = "homebrew:mason"
+    
+    system dart/"pub", "get"
+
+    if Hardware::CPU.is_64_bit?
+      # Build a native-code executable on 64-bit systems only. 32-bit Dart
+      # doesn't support this.
+      system dart/"dart2native", "-Dversion=#{version}", "bin/mason.dart",
+             "-o", "mason"
+      bin.install "mason"
+    else
+      system dart/"dart",
+             "-Dversion=#{version}",
+             "--snapshot=mason.dart.app.snapshot",
+             "--snapshot-kind=app-jit",
+             "bin/mason.dart", "version"
+      lib.install "mason.dart.app.snapshot"
+    end
+  end
+end
