f27b8be2568239924e3f70f073f8bd2aff9ab207
Felix Angelov <felangelov@gmail.com>
2021-06-09 00:23:36 -0500

    chore: v0.0.1-dev.38

--- mason.rb
+++ mason.rb
@@ -1,8 +1,8 @@
 class Mason < Formula
   desc "A template generator which helps teams generate files quickly and consistently."
   homepage "https://pub.dev/packages/mason"
-  url "https://github.com/felangel/mason/archive/refs/tags/v0.0.1-dev.37.tar.gz"
-  sha256 "a6a2bf7ae5d23667552787c705879478f888b10eab9d26cb6ccd9d6070923e11"
+  url "https://github.com/felangel/mason/archive/refs/tags/v0.0.1-dev.38.tar.gz"
+  sha256 "b02817c90ab14f195d8a29779318d9c75ddd34b1ffbc168415fc0351b9f49c6b"
   license "MIT"
 
   depends_on "dart"
