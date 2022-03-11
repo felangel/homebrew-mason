1eb1e63a00aa79e1c91778a2d0de964c52c744f8
Felix Angelov <felangelov@gmail.com>
2022-03-10 22:38:43 -0600

    fix: installation for M1

--- mason.rb
+++ mason.rb
@@ -1,39 +1,62 @@
+require "yaml"
+
 class Mason < Formula
   desc "A template generator which helps teams generate files quickly and consistently."
-  homepage "https://pub.dev/packages/mason_cli"
+  homepage "https://github.com/felangel/mason"
   url "https://github.com/felangel/mason/archive/refs/tags/mason_cli-v0.1.0-dev.16.tar.gz"
   sha256 "1618a85baa3022d0778e03dd5a7e5bc599019007a9255bcd29afe84cf51068d3"
   license "MIT"
 
-  depends_on "dart"
+  depends_on "dart-lang/dart/dart" => :build
 
   def install
-    dart = Formula["dart"].opt_bin
+    # Tell the pub server where these installations are coming from.
+    ENV["PUB_ENVIRONMENT"] = "homebrew:mason_cli"
 
     # Change directories into the mason_cli package directory.
     Dir.chdir('packages/mason_cli')
 
-    pubspec = YAML.safe_load(File.read("pubspec.yaml"))
-    version = pubspec["version"]
-
-    # Tell the pub server where these installations are coming from.
-    ENV["PUB_ENVIRONMENT"] = "homebrew:mason"
-    
-    system dart/"pub", "get"
-
+    system _dart/"dart", "pub", "get"
+  
+    # Build a native-code executable on 64-bit systems only. 32-bit Dart
+    # doesn't support this.
     if Hardware::CPU.is_64_bit?
-      # Build a native-code executable on 64-bit systems only. 32-bit Dart
-      # doesn't support this.
-      system dart/"dart2native", "-Dversion=#{version}", "bin/mason.dart",
-             "-o", "mason"
-      bin.install "mason"
+      _install_native_executable
     else
-      system dart/"dart",
-             "-Dversion=#{version}",
-             "--snapshot=mason.dart.app.snapshot",
-             "--snapshot-kind=app-jit",
-             "bin/mason.dart", "version"
-      lib.install "mason.dart.app.snapshot"
+      _install_script_snapshot
     end
+
+    chmod 0555, "#{bin}/mason"
+  end
+
+  private
+
+  def _dart
+    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
+  end
+
+  def _version
+    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
+  end
+
+  def _install_native_executable
+    system _dart/"dart", "compile", "exe", "-Dversion=#{_version}",
+           "bin/mason.dart", "-o", "mason"
+    bin.install "mason"
+  end
+
+  def _install_script_snapshot
+    system _dart/"dart", "compile", "jit-snapshot",
+           "-Dversion=#{_version}",
+           "-o", "mason.dart.app.snapshot",
+           "bin/mason.dart"
+    lib.install "mason.dart.app.snapshot"
+    
+    cp _dart/"dart", lib
+
+    (bin/"mason").write <<~SH
+      #!/bin/sh
+      exec "#{lib}/dart" "#{lib}/mason.dart.app.snapshot" "$@"
+    SH
   end
 end
