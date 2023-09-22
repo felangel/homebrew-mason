ad53cb8b0041de733dd4819ed6685e12da13ece4
Felix Angelov <felangelov@gmail.com>
2023-09-22 00:52:07 -0500

    feat: mason_cli v0.1.0-dev.52

--- mason.rb
+++ mason.rb
@@ -3,8 +3,8 @@ require "yaml"
 class Mason < Formula
   desc "A template generator which helps teams generate files quickly and consistently."
   homepage "https://github.com/felangel/mason"
-  url "https://github.com/felangel/mason/archive/refs/tags/mason_cli-v0.1.0-dev.51.tar.gz"
-  sha256 "5d412932869d5ed3f1a80d7b043c9d7f664dd693c413920ef223140ed145d905"
+  url "https://github.com/felangel/mason/archive/refs/tags/mason_cli-v0.1.0-dev.52.tar.gz"
+  sha256 "2d06afc52ed2f7e3e506911652125e19da547d08be83d1cacc74a544161ed165"
   license "MIT"
 
   depends_on "dart-lang/dart/dart" => :build
